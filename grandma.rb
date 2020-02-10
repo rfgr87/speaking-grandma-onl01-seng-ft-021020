ea2707f666eca72d67084cc435eb80c277dee4d4
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-10 16:28:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 
 def speak_to_grandma(string)
   if string != string.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase
-    puts "NO, NOT SINCE 1938"
+    return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
     
